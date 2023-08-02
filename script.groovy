e8fa8d493492df894957b3e2e323b4bc34b9d4bd
krnmalde2 <krnmalde2@gmail.com>
2023-08-02 16:42:08 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -8,7 +8,7 @@ def buildImage() {
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t krnmalde/jenkins:3.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push krnmalde/jenkis:3.0'
+        sh 'docker push krnmalde/jenkins:3.0'
     }
 } 
 
