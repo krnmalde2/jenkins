6c972e9da876b9bcc65608f9bbb11af4d14e852a
karan <kar@gmail.com>
2023-08-02 17:35:46 +0530

    added my code

---
+++ script2.groovy
@@ -0,0 +1,20 @@
+def buildjar(){
+    sh 'mvn package'
+}
+
+def deployment(){
+    withCredentials([usernamePassword(
+        credentialsId:'dockerhub repo',
+        usernameVariable:'USER',
+        passwordVariable:'PASS'
+    )])
+    {
+        sh 'docker build -t krnmalde/jenkins:5.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push krnmalde/jenkins:5.0'
+    }
+
+
+}
+
+return this
